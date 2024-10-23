0ea610b22f26e3fa10bb7dfb53733447ce7cf642
sromano87 <siromano@unisa.it>
2024-10-23 15:58:32 +0200

    Red phase of US1

--- test/test_translator.py
+++ test/test_translator.py
@@ -1,8 +1,14 @@
 from unittest import TestCase
 
+from src.translator import PigLatinTranslator
+
 
 class TestPigLatinTranslator(TestCase):
 
-    def test_something(self):
-        # Test method example
-        pass
+    def test_get_phrase(self):
+        # Arrange
+        translator = PigLatinTranslator("hello world")
+        # Act
+        phrase = translator.get_phrase()
+        # Assert
+        self.assertEqual("hello world", phrase)
