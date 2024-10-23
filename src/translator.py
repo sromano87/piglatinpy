35c52cfe6e4c85d0fe328f6a966cd5a4df650e72
sromano87 <siromano@unisa.it>
2024-10-23 17:23:58 +0200

    Refactor phase of US5

--- src/translator.py
+++ src/translator.py
@@ -27,30 +27,37 @@ class PigLatinTranslator:
         """
         if self._phrase == "":
             return "nil"
-        first_char = self._phrase[0]
+        return PigLatinTranslator._translate_word(self._phrase)
+
+
+    @staticmethod
+    def _translate_word(word: str) -> str:
+        first_char = word[0]
         if first_char in VOWELS:
-            return self._translate_word_starting_vowel()
+            return PigLatinTranslator._translate_word_starting_vowel(word)
         elif first_char in CONSONANTS:
-            return self._translate_word_starting_consonant()
+            return PigLatinTranslator._translate_word_starting_consonant(word)
         else:
             raise PigLatinError
 
-    def _translate_word_starting_vowel(self) -> str:
-        last_char = self._phrase[-1]
+    @staticmethod
+    def _translate_word_starting_vowel(word) -> str:
+        last_char = word[-1]
         if last_char == "y":
-            return self._phrase + "nay"
+            return word + "nay"
         if last_char in VOWELS:
-            return self._phrase + "yay"
+            return word + "yay"
         if last_char in CONSONANTS:
-            return self._phrase + "ay"
+            return word + "ay"
         else:
             raise PigLatinError
 
-    def _translate_word_starting_consonant(self) -> str:
+    @staticmethod
+    def _translate_word_starting_consonant(word) -> str:
         suffix = ""
-        for char in self._phrase:
+        for char in word:
             if char in CONSONANTS:
                 suffix += char
             else:
                 break
-        return self._phrase[len(suffix):] + suffix + "ay"
+        return word[len(suffix):] + suffix + "ay"
